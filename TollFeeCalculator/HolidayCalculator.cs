b5ad725ec04b2735d6247a3419c2ff9bf82107e6
Bence Ludmány <bence.ludmany@singula.se>
2023-12-18 12:34:53 +0100

    Impl påsk i HolidayCalculator

--- TollFeeCalculator/HolidayCalculator.cs
+++ TollFeeCalculator/HolidayCalculator.cs
@@ -1,3 +1,5 @@
+using System.Security.Cryptography;
+
 namespace TollFeeCalculator;
 
 public class HolidayCalculator
@@ -19,6 +21,31 @@ public class HolidayCalculator
 
     public bool IsEasterRelatedHoliday(DateTime date)
     {
-        return false;
+        var easter = CalculateEaster(date.Year);
+        var easterMonday = easter.AddDays(1);
+        var goodFriday = easter.AddDays(-2);
+        var ascensionDay = easter.AddDays(39);
+        var pentecost = easter.AddDays(49);
+
+        return date == easter || date == easterMonday || date == goodFriday || date == ascensionDay || date == pentecost;
+    }
+
+    private DateTime CalculateEaster(int year)
+    {
+        var a = year % 19;
+        var b = year / 100;
+        var c = year % 100;
+        var d = b / 4;
+        var e = b % 4;
+        var f = (8 * b + 13) / 25;
+        var g = (19 * a + b - d - f + 15) % 30;
+        var h = (a + 11 * g) / 319;
+        var i = c / 4;
+        var j = c % 4;
+        var k = (2 * e + 2 * i - j - g + h + 32) % 7;
+        var month = (g - h + k + 90) / 25;
+        var day = (g - h + k + month + 19) % 32;
+
+        return new DateTime(year, month, day);
     }
 }
