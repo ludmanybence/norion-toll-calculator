d982443818575300aaa28bd65b5e95625c0e8b43
Bence Ludmány <bence.ludmany@singula.se>
2023-12-18 13:45:44 +0100

    Impl IsTollFreeDate

--- TollFeeCalculator/TollCalculator.cs
+++ TollFeeCalculator/TollCalculator.cs
@@ -61,7 +61,7 @@ public class TollCalculator
             DateTime intervalEnd = new(today.Year, today.Month, today.Day, item.Interval.End.Hour, item.Interval.End.Minute, 0);
             DateTime intervalStart = new(today.Year, today.Month, today.Day, item.Interval.Start.Hour, item.Interval.Start.Minute, 0);
 
-            if(timeOfDay <= intervalEnd && timeOfDay >= intervalStart)
+            if (timeOfDay <= intervalEnd && timeOfDay >= intervalStart)
             {
                 price = item.Price;
             }
@@ -84,26 +84,6 @@ public class TollCalculator
 
     private bool IsTollFreeDate(DateTime date)
     {
-        int year = date.Year;
-        int month = date.Month;
-        int day = date.Day;
-
-        if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday) return true;
-
-        if (year == 2013)
-        {
-            if (month == 1 && day == 1 ||
-                month == 3 && (day == 28 || day == 29) ||
-                month == 4 && (day == 1 || day == 30) ||
-                month == 5 && (day == 1 || day == 8 || day == 9) ||
-                month == 6 && (day == 5 || day == 6 || day == 21) ||
-                month == 7 ||
-                month == 11 && day == 1 ||
-                month == 12 && (day == 24 || day == 25 || day == 26 || day == 31))
-            {
-                return true;
-            }
-        }
-        return false;
+        return date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday || HolidayCalculator.IsHoliday(date) || HolidayCalculator.IsHoliday(date.AddDays(1));
     }
 }
