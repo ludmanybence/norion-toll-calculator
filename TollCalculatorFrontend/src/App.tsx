176e468ce9a8dc273a0aac9f3f0ce00d92b865cd
Bence Ludmány <bence.ludmany@singula.se>
2023-12-18 18:15:25 +0100

    Add tailwind, add button with an API call on click, display result

--- TollCalculatorFrontend/src/App.tsx
+++ TollCalculatorFrontend/src/App.tsx
@@ -1,5 +1,38 @@
+import { useEffect, useState } from "react";
+import "./index.css";
+
 function App() {
-  return <>Toll Calculator</>;
+  const [vehicleType, setVehicleType] = useState();
+
+  const [passages, setPassages] = useState<Date[]>([]);
+
+  useEffect(() => {
+    setPassages([]);
+  }, [vehicleType]);
+
+  const [resultingPrice, setResultingPrice] = useState();
+
+  const onGetTotalButtonClick = () => {
+    fetch("/api/toll", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(passages),
+    })
+      .then((response) => response.json())
+      .then((data) => setResultingPrice(data.price));
+  };
+
+  return (
+    <div className=" min-h-screen flex flex-col justify-center items-center">
+      {resultingPrice}
+      <button
+        className="bg-blue-500 hover:bg-blue-400 duration:50 transition p-5 rounded text-white"
+        onClick={onGetTotalButtonClick}
+      >
+        Get total
+      </button>
+    </div>
+  );
 }
 
 export default App;
