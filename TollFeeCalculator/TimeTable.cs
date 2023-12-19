b4b1271c70bb30d377f3b73ac93395c30e15d4b9
Bence Ludmány <bence.ludmany@singula.se>
2023-12-19 10:21:11 +0100

    Patcha oavsiktlig betalfri period mellan 16:30-16:59

--- TollFeeCalculator/TimeTable.cs
+++ TollFeeCalculator/TimeTable.cs
@@ -13,7 +13,7 @@ public static class TimeTable
         new (Price:13, Interval:new (Start:new (8,0), End:new (8,29))),
         new (Price:8, Interval:new (Start:new (8,30), End:new (14,59))),
         new (Price:13, Interval:new (Start:new (15,0), End:new (15,29))),
-        new (Price:18, Interval:new (Start:new (15,30), End:new (16,29))),
+        new (Price:18, Interval:new (Start:new (15,30), End:new (16,59))),
         new (Price:13, Interval:new (Start:new (17,0), End:new (17,59))),
         new (Price:8, Interval:new (Start:new (18,0), End:new (18,29))),
         ];
